1303d3719b1910770671aa729993fb5602d75ef7
Liam Kelly <liamk024@gmail.com>
2025-12-27 22:27:04 +0000

    Add template docker-compose.yml & sample_cog.py

---
+++ sample_cog.py
@@ -0,0 +1,28 @@
+import discord, config
+from discord.ext import commands
+from discord import app_commands
+
+# Main class for checking website status'
+class CogTemplate(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.tree.add_command(self.cog_template, guilds=self.bot.active_guilds)
+
+        self.bot.console_log('CogTemplate cog loaded successfully.')
+
+    # Define cog_template command group
+    cog_template = app_commands.Group(
+        name='cog_template',
+        description='Template Command'
+    )
+    
+    # Add website command
+    @cog_template.command(name='template', description='Template command')
+    async def add_website(self, interaction: discord.Interaction, url: str, channel: discord.TextChannel):
+        self.bot.console_log(f'\'{interaction.user.name}\' used the command /cog_template')
+
+        await interaction.response.send_message(f'Hello, World!', ephemeral=True)
+
+# Setup function to load cog into bot
+async def setup(bot):
+    await bot.add_cog(Cog(bot))
